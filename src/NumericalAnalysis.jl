2059ed317233a8a2f1776704560c4d59b4c49b79
ZhuoFei Zhou <fregot@icloud.com>
2020-09-26 17:13:28 +0800

    add SEq1.jl
    export Bisection function

--- src/NumericalAnalysis.jl
+++ src/NumericalAnalysis.jl
@@ -1,12 +1,14 @@
 module NumericalAnalysis
 using Documenter
 using ForwardDiff
-include("basic.jl")
 
+include("basic.jl")
 export my_f, derivative_my_f, ξ, absolute_error, relative_error, NthDerivative, TaylorPolynomials
 
 # Write your package code here.
 
+include("SEq1.jl")
 
+export Bisection
 
 end
