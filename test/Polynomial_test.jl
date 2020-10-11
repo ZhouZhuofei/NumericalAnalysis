05cd6060efaaf26908dd28633b459d872ea05c62
ZhuoFei Zhou <fregot@icloud.com>
2020-10-11 18:11:56 +0800

    add test

--- test/Polynomial_test.jl
+++ test/Polynomial_test.jl
@@ -1,2 +1,10 @@
 using NumericalAnalysis: Polynomial
 using Test
+
+x = [1.0, 1.3, 1.6, 1.9, 2.2]
+y = [0.7651977, 0.6200860, 0.4554022, 0.2818186, 0.1103623]
+@testset "Polynomial test" begin
+    @test abs(Polynomial.Lagrange(x,y)(1.5) - 0.5118199)<=0.0000001
+    @test abs(Polynomial.NDDF(x,y)(1.5) - 0.5118199)<=0.0000001
+    @test abs(abs(Polynomial.Neville(x,y,1.5) - 0.5118199)<=0.0000001)
+end
